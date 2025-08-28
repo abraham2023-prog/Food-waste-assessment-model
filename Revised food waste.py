a20503acefd32d46f91b051724f5397575d61eac
Abraham Abraham Habtemichael Tesfamariam <engabraham23@gmail.com>
2025-08-28 15:14:14 +0700

    Update Revised food waste.py

--- Revised food waste.py
+++ Revised food waste.py
@@ -33,7 +33,7 @@ def generate_key(prefix, data=None):
 # Set page configuration
 st.set_page_config(
     page_title="Food Waste Analysis Dashboard",
-    page_icon="🍎🍽️🥦>🗑️",
+    page_icon="🍽️🥦🗑️",
     layout="wide",
     initial_sidebar_state="expanded"
 )
